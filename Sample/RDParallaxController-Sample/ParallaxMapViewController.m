abdbc59eaddb33d7b5e05f9dfdc97734f5dbcf96
ReDetection <redetection@gmail.com>
2014-07-20 15:16:57 +0700

    placed all subviews of ParallaxMapViewController directly inside controller, removed unneeded viewControllers

--- Sample/RDParallaxController-Sample/ParallaxMapViewController.m
+++ Sample/RDParallaxController-Sample/ParallaxMapViewController.m
@@ -8,12 +8,12 @@
 
 #import <MapKit/MapKit.h>
 #import "ParallaxMapViewController.h"
-#import "SampleScrollViewController.h"
 #import "RDParallaxController.h"
 
 @interface ParallaxMapViewController ()
 @property (nonatomic, weak) IBOutlet RDParallaxController *parallaxController;
 @property (nonatomic, weak) IBOutlet MKMapView *mapView;
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
 @end
 
 @implementation ParallaxMapViewController
@@ -21,14 +21,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    SampleScrollViewController *sampleBottomViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"SampleScrollViewController"];
-
-    [self.parallaxController setupWithTopView:self.mapView topHeight:200 bottomView:sampleBottomViewController.scrollView];
+    [self.parallaxController setupWithTopView:self.mapView topHeight:200 bottomView:self.scrollView];
     self.parallaxController.fullHeight = self.view.frame.size.height-50.0f;
 }
 
-- (IBAction) dismiss:(id)sender{
-    [self dismissViewControllerAnimated:YES completion:nil];
+- (IBAction)dismiss:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
