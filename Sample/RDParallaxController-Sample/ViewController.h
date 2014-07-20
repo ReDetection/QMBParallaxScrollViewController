c2c6a7c195d305eb5d02b5e05106f08b62d69291
ReDetection <redetection@gmail.com>
2014-07-20 13:46:54 +0700

    directly place topView and own it fom main ViewController

--- Sample/RDParallaxController-Sample/ViewController.h
+++ Sample/RDParallaxController-Sample/ViewController.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "SampleTopViewController.h"
 
 @interface ViewController : UIViewController
 
--- Sample/RDParallaxController-Sample/ViewController.m
+++ Sample/RDParallaxController-Sample/ViewController.m
@@ -12,7 +12,7 @@
 
 @interface ViewController ()<QMBParallaxScrollViewControllerDelegate>
 @property (nonatomic, weak) IBOutlet RDParallaxController *parallaxController;
-@property (nonatomic, strong) UIViewController *topViewController;
+@property (nonatomic, weak) IBOutlet UIImageView *topView;
 @property (nonatomic, strong) SampleTableViewController *sampleTableViewController;
 @end
 
@@ -21,17 +21,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //TODO get rid of topViewController, place that view into storyboard directly
-    
     self.sampleTableViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"SampleTableViewController"];
     
-    self.topViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"SampleTopViewController"];
     self.parallaxController.delegate = self;
     
-    [self.view addSubview:self.sampleTableViewController.tableView];
-    [self.view addSubview:self.topViewController.view];
+    [self.view insertSubview:self.sampleTableViewController.tableView belowSubview:self.topView];
 
-    [self.parallaxController setupWithTopView:self.topViewController.view topHeight:200 bottomView:self.sampleTableViewController.tableView];
+    [self.parallaxController setupWithTopView:self.topView topHeight:200 bottomView:self.sampleTableViewController.tableView];
     self.parallaxController.overPanHeight = 250;
 
     if ([UIDevice currentDevice].systemVersion.floatValue >= 7) {
@@ -45,14 +41,8 @@
 
 #pragma mark - QMBParallaxScrollViewControllerDelegate
 
-- (void)parallaxScrollViewController:(RDParallaxController *)controller didChangeState:(QMBParallaxState)state {
-    NSLog(@"didChangeState %d",state);
-    [self.navigationController setNavigationBarHidden:state == QMBParallaxStateFullSize animated:YES];
-    
-}
-
 - (void)parallaxScrollViewController:(RDParallaxController *)controller didChangeTopHeight:(CGFloat)height {
-    [self.topViewController.view setAlpha:MAX(.7f,height/self.parallaxController.fullHeight)];
+    [self.topView setAlpha:MAX(.7f,height/self.parallaxController.fullHeight)];
 }
 
 - (void)parallaxScrollViewController:(RDParallaxController *)controller didChangeGesture:(QMBParallaxGesture)newGesture oldGesture:(QMBParallaxGesture)oldGesture {
